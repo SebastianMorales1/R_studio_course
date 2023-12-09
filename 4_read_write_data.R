b6a9ce309a347e72cba13ecbaf2996522c28406d
SebastianMorales1 <sebastian.morales03@est.uexternado.edu.co>
2023-12-09 18:05:45 -0500

    Last changes in the 4° exercise

--- 4_read_write_data.R
+++ 4_read_write_data.R
@@ -8,7 +8,8 @@
 #It can import text files, other statistic soft wares, spreadsheets, etc.
 #It can specify the location (online/ offline) from an URL.
 
-#Different variabes are separated with a special character, callerd "Delimitator".
+#Different variabes are separated with a special character, called "Delimitator".
+#In other cases, variables are differentiated by their location in each line.
 
 ###Functions to read data in R.####
 ?read.table
@@ -63,7 +64,8 @@ load()
 unserialize()
 
 
-###Existen funcines para escribir datos en R.####
+
+###Existen funciones para escribir datos en R.####
 write.table()
 #La funcion para escribir datos por defecto.
 #Tambien tiene funciones especializadas como read.table.
@@ -76,38 +78,54 @@ save
 serialize
 
 
-####Ejercicio practico de unidad 4####
+###Practical exercise of the 4° section###
 datos <- read.table("swirl_temp/inmigintnalpry.csv",header=TRUE, sep=",", 
                     fileEncoding = "latin1"
-                    #Se usa para abarcar mas simbolos que no usan otros idiomas
-                    #Por ejemplo, la "ñ"
+                    #"fileEnconding" is used to accept other symbols from other languages
+                    #For instance, the "ñ" in Spanish.
                     )
-View(datos) #Para ver la base de datos
+View(datos) #To see the dataset.
 
 datos_2 <- read.table("swirl_temp/inmigintnalpry.csv", header=TRUE, sep=",")
-#RESULTADO de "datos_2"
+#RESULTS from dataset "datos_2"
 #Error in make.names(col.names, unique = TRUE) : 
  # invalid multibyte string 2
+#This is an error, because R does not understand the symbol "ñ" from the spanish word "año".
 
-# read.cvs2() y read.delim2()
-# Si usas comas para indicar punto decimal en los números.
+# read.cvs()
+#If you use commas to indicate the decimal point in numbers.
 
-#Agilizar la lectura de datos con el parametro "colClasses"
+# read.delim()
+#If you have files that are delimitated by TAB without specify other options.
+
+#If you are in Europe you can use commas to indicate decimal point in numbers.
+#Using read.cvs2() or read.delim2()
+
+#You can accelerate the data reading with the parametre "colClasses"
+#Two important options are sep and header.
 inicial <- read.table("swirl_temp/inmigintnalpry.csv",header=TRUE, sep=",", fileEncoding = "latin1", nrows = 100)
+#Because the dataset is too large, we use "colClasses to select the first 100 rows of the file"
 clases <- sapply(inicial, class)
+#With the line above, we identify the class of the columns in the dataset.
 print(clases)
-datos <- read.table("swirl_temp/inmigintnalpry.csv",header=TRUE, sep=",", fileEncoding = "latin1", colClasses = clases)
-#Se usa save() para guardar objetos.
-save(datos, clases, file = "swirl_temp/datos_inmigrates.RData")
-# El argumento file debe ser explicitamente nombrado.
-save(datos,file="C:/Documents and Settings/Mi Usuario/Mis Documentos/datos_inmigrates.RData")
 
-#Para borrar objetos, se usa rm()
+datos <- read.table("swirl_temp/inmigintnalpry.csv",header=TRUE, sep=",", fileEncoding = "latin1", colClasses = clases)
+#We use save() to save multiple objects in the same file.
+save(datos, clases, file = "swirl_temp/datos_inmigrates.RData")
+
+save(datos,file="C:/Documents and Settings/Mi Usuario/Mis Documentos/datos_inmigrates.RData")
+#The file argument must be named explicitly.
+#save(datos, file="C:/programación/Rstudio/R_studio_course/")
+#It is my personal location of the file, nothing important.
+
+#To delete objects, we use rm()
 rm(datos,clases)
-#Se usa ls() para ver que conjunto de datos y funciones el usuario ha definido.
+
+# ls() is used to watch the dataset and the functions the user has definde previously.
 ls()
 load("swirl_temp/datos_inmigrates.RData")
 
-#Para exportar (escribir) datos se usa la funcion "write.table"
-#Generalmente se exportan data frames y matrices.
+#To export (write) data, we use the function "write.table".
+# And the name of the object as an argument.
+#Normally, it is exported data frames and matrices.
 write.table(datos, file="swirl_temp/datos.txt")
